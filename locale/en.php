55a2af6be092488b568871ae4c87b053b160db26
R-J <robin.jurinka@gmx.net>
2019-10-14 12:49:48 +0200

    Fix description

---
+++ locale/en.php
@@ -0,0 +1,3 @@
+<?php
+
+$Definition['rj-spotify-instructions'] = 'Configure your forum to connect with Spotify by filling in your unique client ID and client secret. You will need to add <code>%s</code> as a white-listed redirect url in <a href="https://developer.spotify.com/dashboard">Spotifys dashboard</a>';
