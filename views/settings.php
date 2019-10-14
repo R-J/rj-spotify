2c9f60d53b3b07a216a6d52a68c214ab8422d3bd
R-J <robin.jurinka@gmx.net>
2019-10-14 12:29:24 +0200

    Create custom view to only show needed fields (ID and secret)

--- views/settings.php
+++ views/settings.php
@@ -1,10 +1,35 @@
-<h1><?php echo $this->data('Title'); ?></h1>
+<?php defined('APPLICATION') or die;
+$formData = $this->data('_Form');
+?>
+<h1><?= $this->data('Title') ?></h1>
 
-<div class="padded alert alert-warning">
-    <?php echo sprintf(t('oauth2Instructions'), $this->data('redirectUrls')); ?>
+<div class="padded alert alert-info">
+    <?= $this->data('Description') ?>
 </div>
-<?php
-echo $this->Form->open();
-echo $this->Form->errors();
-echo $this->Form->simple($this->data('_Form'));
-echo $this->Form->close('Save');
+<?= $this->Form->open(), $this->Form->errors() ?>
+<ul>
+    <li class="form-group">
+        <div class="label-wrap">
+            <?= $this->Form->label($formData['AssociationKey']['LabelCode'], 'AssociationKey') ?>
+            <div class="description info"><?= Gdn::translate($formData['AssociationKey']['Description']) ?></div>
+        </div>
+        <?= $this->Form->inputWrap('AssociationKey') ?>
+    </li>
+    <li class="form-group">
+        <div class="label-wrap">
+            <?= $this->Form->label($formData['AssociationSecret']['LabelCode'], 'AssociationSecret') ?>
+            <div class="description info"><?= Gdn::translate($formData['AssociationSecret']['Description']) ?></div>
+        </div>
+        <?= $this->Form->inputWrap('AssociationSecret') ?>
+    </li>
+    <li class="Hidden">
+        <?= $this->Form->textBox('AuthorizeUrl', ['value' => $this->data('AuthorizeUrl')]) ?>
+    </li>
+    <li class="Hidden">
+        <?= $this->Form->textBox('TokenUrl', ['value' => $this->data('TokenUrl')]) ?>
+    </li>
+    <li class="Hidden">
+        <?= $this->Form->textBox('ProfileUrl', ['value' => $this->data('ProfileUrl')]) ?>
+    </li>
+</ul>
+<?= $this->Form->close('Save') ?>
