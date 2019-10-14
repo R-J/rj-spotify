f218ad02cc1608cfd704dc89292401d2b6251cf5
R-J <robin.jurinka@gmx.net>
2019-10-14 16:14:35 +0200

    Simplify settings view

--- views/settings.php
+++ views/settings.php
@@ -1,35 +1,25 @@
 <?php defined('APPLICATION') or die;
+// Get form data.
 $formData = $this->data('_Form');
+// Remove fields that need not be configured by the user.
+unset($formData['AuthorizeUrl']);
+unset($formData['TokenUrl']);
+unset($formData['ProfileUrl']);
+unset($formData['BearerToken']);
+// Rephrase form element text.
+$formData['IsDefault']['Description'] = $formData['IsDefault']['LabelCode'];
+$formData['IsDefault']['LabelCode'] = 'Default Signin Method';
 ?>
 <h1><?= $this->data('Title') ?></h1>
-
 <div class="padded alert alert-info">
     <?= sprintf($this->data('Description'), $this->data('RedirectUrl')) ?>
 </div>
-<?= $this->Form->open(), $this->Form->errors() ?>
-<ul>
-    <li class="form-group">
-        <div class="label-wrap">
-            <?= $this->Form->label($formData['AssociationKey']['LabelCode'], 'AssociationKey') ?>
-            <div class="description info"><?= Gdn::translate($formData['AssociationKey']['Description']) ?></div>
-        </div>
-        <?= $this->Form->inputWrap('AssociationKey') ?>
-    </li>
-    <li class="form-group">
-        <div class="label-wrap">
-            <?= $this->Form->label($formData['AssociationSecret']['LabelCode'], 'AssociationSecret') ?>
-            <div class="description info"><?= Gdn::translate($formData['AssociationSecret']['Description']) ?></div>
-        </div>
-        <?= $this->Form->inputWrap('AssociationSecret') ?>
-    </li>
-    <li class="Hidden">
-        <?= $this->Form->textBox('AuthorizeUrl', ['value' => $this->data('AuthorizeUrl')]) ?>
-    </li>
-    <li class="Hidden">
-        <?= $this->Form->textBox('TokenUrl', ['value' => $this->data('TokenUrl')]) ?>
-    </li>
-    <li class="Hidden">
-        <?= $this->Form->textBox('ProfileUrl', ['value' => $this->data('ProfileUrl')]) ?>
-    </li>
-</ul>
+<?= $this->Form->open() ?>
+<?= $this->Form->errors() ?>
+<?= $this->Form->simple($formData) ?>
+<p class="Hidden">
+    <?= $this->Form->textBox('AuthorizeUrl', ['value' => $this->data('AuthorizeUrl')]) ?>
+    <?= $this->Form->textBox('TokenUrl', ['value' => $this->data('TokenUrl')]) ?>
+    <?= $this->Form->textBox('ProfileUrl', ['value' => $this->data('ProfileUrl')]) ?>
+</p>
 <?= $this->Form->close('Save') ?>
